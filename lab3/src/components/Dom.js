aa250b0ad9f839b430e153ec3a3bea56ab0c94cd
jung-630112418006 <630112418006@bru.ac.th>
2021-12-18 12:13:26 +0700

    lab3

---
+++ lab3/src/components/Dom.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const age = 14;
+
+const dom= (
+<>
+  <h1>Hello React !!!</h1>
+  <p>My Chinnawat Kleebmuang ,{age} years old.</p>
+</>
+
+);
+
+function Dom(){
+    return dom;
+}
+export default Dom
